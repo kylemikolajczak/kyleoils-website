eed968de3a486aaa0f2a5ea87244c17fd9ffadb1
Kyle Daniel Mikolajczak <kyle.mikolajczak@gmail.com>
2026-07-19 16:57:58 +0200

    Add Astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://kyleoils.com',
   output: 'static',
   trailingSlash: 'always',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.endsWith('/404/'),
+    }),
+  ],
 });
